911c4faa79c658bc49785a0d97a0c0fd39cf7566
dennis1996 <dennis.wiersma@hotmail.nl>
2019-09-12 15:49:01 +0100

    Initial commit

---
+++ app/src/main/java/com/example/placesofinterest/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.placesofinterest
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_place.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        rvPlaces.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        rvPlaces.adapter = placeAdapter
+
+        // Populate the places list and notify the data set has changed.
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanges()
+    }
+}
